d318875d5e899dd943a68f81a5aa3e7ff0d026f7
Wellington Costa <wcosta.ale@gmail.com>
2018-08-31 16:41:32 -0300

    Projeto base

---
+++ criptografia/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace criptografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxEntrada = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxSaida = new System.Windows.Forms.RichTextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxEntrada
+            // 
+            this.richTextBoxEntrada.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxEntrada.Location = new System.Drawing.Point(12, 25);
+            this.richTextBoxEntrada.Name = "richTextBoxEntrada";
+            this.richTextBoxEntrada.Size = new System.Drawing.Size(331, 96);
+            this.richTextBoxEntrada.TabIndex = 0;
+            this.richTextBoxEntrada.Text = "";
+            // 
+            // richTextBoxSaida
+            // 
+            this.richTextBoxSaida.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxSaida.Location = new System.Drawing.Point(12, 156);
+            this.richTextBoxSaida.Name = "richTextBoxSaida";
+            this.richTextBoxSaida.Size = new System.Drawing.Size(331, 96);
+            this.richTextBoxSaida.TabIndex = 2;
+            this.richTextBoxSaida.Text = "";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOk.Location = new System.Drawing.Point(268, 127);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 1;
+            this.buttonOk.Text = "Encrip";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Texto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Criptografia";
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(268, 258);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpar.TabIndex = 5;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 293);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.richTextBoxSaida);
+            this.Controls.Add(this.richTextBoxEntrada);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Criptografia de César";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxEntrada;
+        private System.Windows.Forms.RichTextBox richTextBoxSaida;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonLimpar;
+    }
+}
+
---
+++ criptografia/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;                       /***********************************/
+using System.Collections.Generic;   /** Autor: Wellington             **/
+using System.ComponentModel;        /** Data criação: 12/03/2012      **/
+using System.Data;                  /***********************************/
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace criptografia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            char[] alfabeto = new char[26] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+          
+            int deslocamento = 3;
+            string cripto = "";
+          
+            //recebe a entrada do texto informado
+            string texto = richTextBoxEntrada.Text.ToLower();
+           
+            for (int i = 0; i < texto.Length; i++)
+            {
+                for (int j = 0; j < alfabeto.Length; j++)
+                {
+                    //verfica se a letra é igual a do alfabeto
+                    if (texto[i] == alfabeto[j])
+                    {
+                        //aplica o algoritmo
+                        int posicao = (j + deslocamento) % 26;
+                        //pega a letra do resultado do algoritmo
+                        cripto += alfabeto[posicao].ToString();
+                    }
+                    //se no texto conter um '<espaço>' adciona 
+                    //esse espaço na resposta da cripto
+                    if (texto[i] == ' ')
+                    {
+                        j = 26;
+                        cripto += " ";
+                    }
+                }
+            }
+
+            richTextBoxSaida.Text = cripto;
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            richTextBoxEntrada.Text = "";
+            richTextBoxSaida.Text = "";
+        }
+        
+    }
+}
